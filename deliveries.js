3f30372fc6827b3e6f0fa1a1dfa155f21b34020f
ajebohustlers11 <cosrxservice@gmail.com>
2025-09-05 10:23:24 -0400

    Update deliveries.js

--- deliveries.js
+++ deliveries.js
@@ -36,8 +36,47 @@ const deliveries = [
       agentName: "Jason Moore",
       shipmentType: "Air"
     },
+
+
+
+      {
+    trackingCode: "R654RFE", 
+    estimatedDelivery: "September 09, 2025",
+    customs: "",
+    shipper: {
+      name: "Laneige",
+      phone: "+1 (929) 330-9752",
+      address: "1407 Broadway, Suite 210, New York, NY 10018",
+      email: " uslaneigecollaboration@gmail.com"
+    },
+    receiver: {
+      name: "Maria Garcia",
+      phone: "+1 (903) 372-8271",
+      address: "3205 duke Ave Big Spring TX 79720",
+    
+    },
+    shipment: {
+      weight: "5.6 kg",
+      courier: "NOVA",
+      packages: "Box",
+      mode: "Air",
+      product: "Pr package",
+      quantity: 20,
+      paymentMode: "Zelle",
+      totalFreight: "$219",
+      carrier: "Air carrier (Nova445763222)",
+      carrierRef: "IT55667788",
+      origin: "USA",
+      destination: "USA",
+      status: "pending",
+      comments: "Package prepared for shipment",
+      agentName: "Jason Moore",
+      shipmentType: "Air"
+    },
+  }
   }
 ];
+
 
 
 
